e4901888bf8c76b377fc70a9a0f56702ad1e5382
Kaleb Currier <currierka@husson.edu>
2022-01-24 16:44:55 -0500

    updated

--- app/calculator.py
+++ app/calculator.py
@@ -1,7 +1,12 @@
 class Calculator:
     
     def get_user_expression(self):
-        expression = input('Welcome to the calculator!\n\n')
-        self.tokens = expression.split(' ')
-        return 'string'
+        self.expression = input('Welcome to the calculator!\n\n')
+
+    def parse_expression_into_tokens(self):
+        self.tokens = self.expression.split(' ')
+
+    def validate_token_data(self):
+        has_at_least_three_tokens = len(self.tokens) >= 3
+        return has_at_least_three_tokens
         
--- app/test_calculator.py
+++ app/test_calculator.py
@@ -1,28 +1,52 @@
 import unittest
 from unittest import mock
 from unittest.mock import patch
+from setuptools import setup
 from calculator import Calculator
 
 class TestCalculator(unittest.TestCase):
 
+    # Use this so I do not have to keep retyping calculator = Calculator(), but I have to put self. in front of any "calculator"
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
     # Use this for verification of instance for the Class
     def test_if_calculator_can_instantiate(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
-    
-    # Use this for STR verification of input and for MOCK input
-    def test_get_user_expression_return_string(self):
-        calculator = Calculator()
-        with unittest.mock.patch('builtins.input', return_value='2 + 2'):
-            test_get_user_expression = calculator.get_user_expression()
-            self.assertIsInstance(test_get_user_expression, str)
-        
+        self.assertIsInstance(self.calculator, Calculator)
 
-    def test_get_user_expression_stores_tokens(self):
-        calculator = Calculator()
+    def test_get_user_expression_stores_tokens(self): 
         with unittest.mock.patch('builtins.input', return_value='2 + 2'):
-            test_get_user_expression_stores_tokens = calculator.get_user_expression()
-            self.assertIsInstance(calculator.tokens, list)
+            self.calculator.get_user_expression()
+            self.assertIsInstance(self.calculator.expression, str)
+
+    def test_parse_expression_into_tokens(self):
+        with unittest.mock.patch('builtins.input', return_value='2 + 2'):
+            self.calculator.get_user_expression()
+            self.calculator.parse_expression_into_tokens()
+            self.assertIsInstance(self.calculator.tokens, list)
+
+    def test_validate_token_data_returns_false_if_less_than_three_tokens(self):
+        with unittest.mock.patch('builtins.input', return_value='+ 2'):
+            self.calculator.get_user_expression()
+            self.calculator.parse_expression_into_tokens()
+        are_tokens_valid = self.calculator.validate_token_data()
+        self.assertFalse(are_tokens_valid)
+
+    def test_validate_token_data(self):
+        with unittest.mock.patch('builtins.input', return_value='2 + 2'):
+            self.calculator.get_user_expression()
+            self.calculator.parse_expression_into_tokens()
+        are_tokens_valid = self.calculator.validate_token_data()
+        self.assertTrue(are_tokens_valid)
+
+    def test_determine_expression_operator(self):
+        pass
+
+    def test_calculate_expression(self):
+        pass
+
+    def test_print_calculated_value(self):
+        pass
 
     
 if __name__ == '__main__':
