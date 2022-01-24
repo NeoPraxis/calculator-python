d97bb8b4ee3745d44f7bd616cda22596249777e5
Kaleb Currier <currierka@husson.edu>
2022-01-24 18:17:01 -0500

    final push :D

--- app/calculator.py
+++ app/calculator.py
@@ -1,12 +1,57 @@
+
 class Calculator:
-    
+    operators = ['*', '/', '+', '-', '%']
+
     def get_user_expression(self):
         self.expression = input('Welcome to the calculator!\n\n')
 
     def parse_expression_into_tokens(self):
         self.tokens = self.expression.split(' ')
 
-    def validate_token_data(self):
-        has_at_least_three_tokens = len(self.tokens) >= 3
+    def validate_token_length(self, tokens):
+        has_at_least_three_tokens = len(tokens) == 3
         return has_at_least_three_tokens
+    
+    def is_number_token(self, token):
+        is_number = token.isnumeric()
+        return is_number
+
+    def is_operator_token(self, token):
+        is_operator = token in self.operators
+        return is_operator
+
+    def is_valid_expression(self, tokens):
+        is_first_token_numeric = self.is_number_token(tokens[0])
+        is_second_token_an_operator = self.is_operator_token(tokens[1])
+        is_third_token_numeric = self.is_number_token(tokens[2])
+        return is_first_token_numeric and is_second_token_an_operator and is_third_token_numeric
+
+    def calculate_value(self, tokens):
+        number1 = float(tokens[0])
+        number2 = float(tokens[2])
+        operator = tokens[1]
+        operations = {
+            '+': lambda x, y: x + y,
+            '-': lambda x, y: x - y,
+            '*': lambda x, y: x * y,
+            '/': lambda x, y: x / y
+        }
+        value = operations[operator](number1, number2)
+        return value
+
+    def print_calculated_value(self, calculated_value):
+        print(f'= {calculated_value}')
+
+    def start_calculator(self):
+        self.get_user_expression()
+        self.parse_expression_into_tokens()
+        is_correct_length = self.validate_token_length(self.tokens)
+        is_valid = is_correct_length and self.is_valid_expression(self.tokens)
+        if is_valid:
+            calculated_value = self.calculate_value(self.tokens)
+            self.print_calculated_value(calculated_value)
         
+
+if __name__ == '__main__':
+    calculator = Calculator()
+    calculator.start_calculator()
--- app/test_calculator.py
+++ app/test_calculator.py
@@ -1,3 +1,4 @@
+import io
 import unittest
 from unittest import mock
 from unittest.mock import patch
@@ -10,43 +11,86 @@ class TestCalculator(unittest.TestCase):
     def setUp(self) -> None:
         self.calculator = Calculator()
 
+    def get_input(self, fake_input):
+        with unittest.mock.patch('builtins.input', return_value=fake_input):
+            self.calculator.get_user_expression()
+    
+    def assert_calculation(self, tokens, value):
+        
+        calculated_value = self.calculator.calculate_value(tokens)
+        self.assertEqual(calculated_value, value)
+            
+
+            
     # Use this for verification of instance for the Class
     def test_if_calculator_can_instantiate(self):
         self.assertIsInstance(self.calculator, Calculator)
 
-    def test_get_user_expression_stores_tokens(self): 
-        with unittest.mock.patch('builtins.input', return_value='2 + 2'):
-            self.calculator.get_user_expression()
-            self.assertIsInstance(self.calculator.expression, str)
+    def test_get_user_expression_stores_tokens(self):
+        self.get_input('2 + 2')
+        self.assertIsInstance(self.calculator.expression, str)
 
     def test_parse_expression_into_tokens(self):
-        with unittest.mock.patch('builtins.input', return_value='2 + 2'):
-            self.calculator.get_user_expression()
-            self.calculator.parse_expression_into_tokens()
-            self.assertIsInstance(self.calculator.tokens, list)
+        self.get_input('2 + 2')
+        self.calculator.parse_expression_into_tokens()
+        self.assertIsInstance(self.calculator.tokens, list)
 
-    def test_validate_token_data_returns_false_if_less_than_three_tokens(self):
-        with unittest.mock.patch('builtins.input', return_value='+ 2'):
-            self.calculator.get_user_expression()
-            self.calculator.parse_expression_into_tokens()
-        are_tokens_valid = self.calculator.validate_token_data()
+    def test_validate_token_length_returns_false_if_less_than_three_tokens(self):
+        self.get_input('+ 2')
+        self.calculator.parse_expression_into_tokens()
+        are_tokens_valid = self.calculator.validate_token_length(self.calculator.tokens)
         self.assertFalse(are_tokens_valid)
 
     def test_validate_token_data(self):
-        with unittest.mock.patch('builtins.input', return_value='2 + 2'):
-            self.calculator.get_user_expression()
-            self.calculator.parse_expression_into_tokens()
-        are_tokens_valid = self.calculator.validate_token_data()
+        self.get_input('2 + 2')
+        self.calculator.parse_expression_into_tokens()
+        are_tokens_valid = self.calculator.validate_token_length(self.calculator.tokens)
         self.assertTrue(are_tokens_valid)
 
-    def test_determine_expression_operator(self):
-        pass
+    def test_is_number_token_returns_false_if_not_a_number(self):
+        is_number = self.calculator.is_number_token('=')
+        self.assertFalse(is_number)
+    
+    def test_if_number_token_returns_true_if_number(self):
+        is_number = self.calculator.is_number_token('1')
+        self.assertTrue(is_number)
+
+    def test_is_operator_token_returns_false_if_not_a_operator(self):
+        is_operator = self.calculator.is_operator_token('p')
+        self.assertFalse(is_operator)
+
+    def test_is_operator_token_returns_true_if_operator(self):
+        is_operator = self.calculator.is_operator_token('*')
+        self.assertTrue(is_operator)
+
+    def test_is_valid_expression_returns_false_if_not_valid(self):
+        tokens = ['awd', 'adw', 'wasd', 'dddddddd', 'baddatablabla']
+        is_valid_expression = self.calculator.is_valid_expression(tokens)
+        self.assertFalse(is_valid_expression)
+
+    def test_is_valid_expression_returns_true_if_valid(self):
+        tokens = ['2', '+', '2']
+        is_valid_expression = self.calculator.is_valid_expression(tokens)
+        self.assertTrue(is_valid_expression)
 
     def test_calculate_expression(self):
-        pass
+        self.assert_calculation(['2', '+', '2'], 4)
+        self.assert_calculation(['3', '+', '3'], 6)
+        self.assert_calculation(['3', '*', '3'], 9)
+        self.assert_calculation(['3', '/', '3'], 1)
 
-    def test_print_calculated_value(self):
-        pass
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_calculated_value(self, mock_stdout):
+        self.calculator.print_calculated_value(69)
+        output = mock_stdout.getvalue()
+        self.assertEqual(output, '= 69\n')
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_start_calculator(self, mock_stdout):
+        with unittest.mock.patch('builtins.input', return_value= '2 + 2'):
+            self.calculator.start_calculator()
+        output = mock_stdout.getvalue()
+        self.assertEqual(output, '= 4.0\n')
 
     
 if __name__ == '__main__':
