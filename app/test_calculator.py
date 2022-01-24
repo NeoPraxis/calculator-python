6c1cac0d25ba2153978be1411575b74814d2d927
Kaleb Currier <currierka@husson.edu>
2022-01-24 15:11:21 -0500

    first commit

---
+++ app/test_calculator.py
@@ -0,0 +1,19 @@
+import unittest
+from calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+
+    def test_if_calculator_can_instantiate(self):
+        calculator = Calculator()
+    
+    def test_user_operator_selection(self):
+        pass
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
